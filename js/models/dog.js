3eb538403b5680d7a27ef6038ce6899255ce12da
ejbirnfam <ericjbirnbaum@gmail.com>
2014-12-14 10:48:23 -0500

    create model and collection example

---
+++ js/models/dog.js
@@ -0,0 +1,21 @@
+Dog = Backbone.Model.extend({
+	intialize: function() {
+		this.on('all', function(eventName, model, msg) {
+			console.log(model.cid+': '+eventName+': '+msg);
+		});
+	},
+	defaults: {
+		fullname: 'unknown',
+		age: 0
+	},
+
+	validate: function(attrs){
+		if (attrs.fullname.length < 1) {
+			return "Name can't be blank";
+		}
+	}
+});
+
+Dogs = Backbone.Collection.extend({
+	model: Dog
+});
